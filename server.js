8e696af8319f200d0b42cd783c28ea4e3ee74728
Varinthorn <earnmaha@hotmail.com>
2025-05-06 01:13:38 +0700

    added the frontend deployed link

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT  
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://frontend-note-app-six.vercel.app"],
     credentials: true
 }))
 
